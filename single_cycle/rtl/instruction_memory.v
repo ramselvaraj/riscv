74f3b6ba1c6cc090f7832e907921bedd26c86256
ramselvaraj <ramelvaraj01@gmail.com>
2026-03-07 14:59:18 -0700

    register file added

--- single_cycle/rtl/instruction_memory.v
+++ single_cycle/rtl/instruction_memory.v
@@ -13,8 +13,9 @@ always @(posedge clk or posedge reset) begin
     for (k = 0; k < 64; k = k+1) begin
       I_Mem[k] <= 32'b0;
     end
+  end
   else
-    inst_out <= I_Mem[read_addr]
+    inst_out <= I_Mem[read_addr];
 end
 
 endmodule //inst_mem
---
+++ single_cycle/rtl/reg_file.v
@@ -0,0 +1,34 @@
+module reg_file(
+  //pass values stored at rreg1 and rreg2 inside the register to rdata1,
+  //rdata2 respectively
+  //
+  //if regwrite control signal is enabled, then write the wdata into register
+  //at position wreg
+  input clk,
+  input reset,
+  input regwrite, 
+  input [4:0] rreg1,  rreg2, wreg,
+  input [31:0] wdata,
+  output reg [31:0] rdata1, rdata2
+)
+
+integer k;
+reg [31:0] registers [31:0];
+assign rdata1 = registers[rreg1];
+assign rdata2 = registers[rreg2];
+
+always @(posedge clk or posedge reset) begin
+  if(reset) begin
+    for (k = 0; k < 32; k = k + 1) begin
+      registers[k] <= 32'b0;
+    end
+  end
+
+  elseif (regwrite) begin
+    registers[wreg] <= wdata;
+  end
+
+end
+
+
+endmodule //reg_file
